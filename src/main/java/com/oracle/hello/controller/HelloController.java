6ee7a4658d03e4901fff9d5e201b9a0ead6d6c80
SiriJeevi <50168796+SiriJeevi@users.noreply.github.com>
2019-06-21 14:24:04 +0530

    Update HelloController.java

--- src/main/java/com/oracle/hello/controller/HelloController.java
+++ src/main/java/com/oracle/hello/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@GetMapping("/")
 	public String hello() {
-		return "Hello world Welcome New World...........Mr. Chiranjeevi";
+		return "Hello world Welcome New World...........Mr. Chiranjeevi ##########";
 	}
 
 }
